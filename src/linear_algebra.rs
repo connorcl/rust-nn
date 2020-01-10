d07c20865cb0a17c736c078fb3db78f6acec3c80
Connor C <connor.c7576@gmail.com>
2020-01-10 17:00:59 +0000

    Implement matmul and transpose and add optional gradients

--- src/linear_algebra.rs
+++ src/linear_algebra.rs
@@ -22,8 +22,10 @@ pub struct Matrix {
     rows: usize,
     /// The number of columns in the matrix
     cols: usize,
-    /// The data contained in the Matrix
+    /// The data contained in the matrix
     data: Vec<f64>,
+    /// An optional matrix of gradients
+    grad: Box<Option<Matrix>>,
 }
 
 impl Matrix {
@@ -35,6 +37,8 @@ impl Matrix {
             cols,
             // set data to zeroes
             data: vec![0.0; rows * cols],
+            // set grad to None
+            grad: Box::new(None),
         }
     }
 
@@ -57,9 +61,33 @@ impl Matrix {
         self.data[index] = val;
     }
 
+    /// Increments the element whose position is specified by the 
+    /// given Coord by the value given
+    pub fn increment_at(&mut self, loc: &Coord, val: f64) {
+        // ensure the coordinates are valid
+        self.validate_coord(&loc);
+        // calculate the correct index within the data vector
+        let index = self.coord_to_index(&loc);
+        // increment the element by the given value
+        self.data[index] += val;
+    }
+
+    /// Initializes the optional matrix of gradients
+    pub fn init_grad(&mut self) {
+        self.grad = Box::new(Some(Matrix::new(self.rows, self.cols)));
+    }
+
+    /// a
+    pub fn get_grad(&self) -> Result<&Matrix, String> {
+        match &*self.grad {
+            Some(grad_matrix) => Ok(&grad_matrix),
+            None => Err(String::from("Matrix of gradients is not set"))
+        }
+    }
+
     /// Return the matrix which is the result of adding the given matrix
     /// to the current matrix
-    pub fn add(&mut self, other: &Matrix) -> Matrix {
+    pub fn add(&self, other: &Matrix) -> Matrix {
         // ensure the given matrix has the correct dimensions
         self.validate_equal_dimensions(other);
         // initialize new matrix
@@ -70,6 +98,42 @@ impl Matrix {
             matrix.data[i] = self.data[i] + other.data[i];
         }
         // return the new matrix
+        matrix
+    }
+
+    /// Return the matrix which is the result of multiplying the given
+    /// matrix with the current matrix
+    pub fn matmul(&self, other: &Matrix) -> Matrix {
+        // ensure given matrix is compatible
+        self.validate_matmul_compatible(other);
+        // create result matrix
+        let mut matrix = Matrix::new(self.rows, other.cols);
+        // current result of element multiplication
+        let mut x: f64;
+        // perform matrix multiplication
+        for i in 0..self.rows {
+            for j in 0..other.cols {
+                for k in 0..self.cols {
+                    x = self.get_at(&Coord::new(i, k)) * 
+                        other.get_at(&Coord::new(k, j));
+                    matrix.increment_at(&Coord::new(i, j), x);
+                }
+            }
+        }
+        matrix
+    }
+
+    /// Returns the transpose of the current matrix
+    pub fn t(&self) -> Matrix {
+        // create result matrix
+        let mut matrix = Matrix::new(self.cols, self.rows);
+        // calculate transposition
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                matrix.set_at(&Coord::new(j, i), 
+                    self.get_at(&Coord::new(i, j)));
+            }
+        }
         matrix
     }
 
@@ -87,12 +151,20 @@ impl Matrix {
         }
     }
 
+    // panic if the given matrix's dimensions do not match the current matrix
+    fn validate_matmul_compatible(&self, other: &Matrix) {
+        if self.cols != other.rows {
+            panic!("Matrices do not have compatible dimensions");
+        }
+    }
+
     // return the index in the data vector corresponding to the given Coord
     fn coord_to_index(&self, coord: &Coord) -> usize {
         coord.row * self.cols + coord.col
     }
 }
 
+// Unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -169,8 +241,57 @@ mod tests {
     #[test]
     #[should_panic(expected = "Matrices do not have equal dimensions")]
     fn elementwise_add_wrong_size() {
-        let mut a = Matrix::new(2, 3);
+        let a = Matrix::new(2, 3);
         let b = Matrix::new(2, 4);
         a.add(&b);
+    }
+
+    // test matrix multiply
+    #[test]
+    fn matrix_multiply() {
+        let mut a = Matrix::new(2, 3);
+        a.data = vec![1., 2., 3., 4., 5., 6.];
+        let mut b = Matrix::new(3, 1);
+        b.data = vec![7., 8., 9.];
+        let c = a.matmul(&b);
+        assert_eq!(c.data, vec![50., 122.]);
+    }
+
+    // test whether matrix multiply panics correctly if matrices
+    // do not have compatible dimensions
+    #[test]
+    #[should_panic(expected = "Matrices do not have compatible dimensions")]
+    fn matrix_multiply_wrong_size() {
+        let a = Matrix::new(2, 3);
+        let b = Matrix::new(2, 3);
+        a.matmul(&b);
+    }
+
+    // test transpose
+    #[test]
+    fn matrix_transposition() {
+        let mut a = Matrix::new(2, 3);
+        a.data = vec![1., 2., 3., 4., 5., 6.];
+        let b = a.t();
+        assert_eq!(b.data, vec![1., 4., 2., 5., 3., 6.]);
+    }
+
+    // test setting of gradients
+    #[test]
+    fn initialize_gradients() {
+        let mut matrix = Matrix::new(2, 3);
+        matrix.init_grad();
+        match *matrix.grad {
+            Some(grad_matrix) => assert_eq!(grad_matrix.data, vec![0.; 6]),
+            None => panic!("Gradients have not been set"),
+        }
+    }
+
+    // test getting gradients returns error if matrix of gradients is not set
+    #[test]
+    #[should_panic]
+    fn get_gradient_matrix() {
+        let matrix = Matrix::new(2, 3);
+        matrix.get_grad().expect("Getting gradients failed");
     }
 }
